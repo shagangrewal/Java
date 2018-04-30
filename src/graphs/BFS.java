0d29405a73a4e1216227a67a3bba515e90fe42c2
Shaganpreet Singh Grewal <grewalshagan94@gmail.com>
2018-04-29 19:07:24 -0500

    New Commit

--- src/graphs/BFS.java
+++ src/graphs/BFS.java
@@ -48,8 +48,11 @@ class BFS {
         g.addEdge(0,1);
         g.addEdge(1,0);
         g.addEdge(1,2);
+        g.addEdge(2,1);
         g.addEdge(2,3);
+        g.addEdge(3,2);
         g.addEdge(3,4);
+        g.addEdge(4,3);
         
         System.out.println("BFS traversal starting at Node 1:");
         bfSearch(1);
